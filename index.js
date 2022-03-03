7e875d5abb31450241be13054f98f51fcd545b19
Michael Gately <michaedavidgately@gmail.com>
2022-03-03 12:59:07 +0100

    fixed page size issue, adding more content

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-
 function switchScreen(screen) {
-         document.querySelector("#top").classList.add("invisible");
-         document.querySelector("#abo").classList.add("invisible");
-         document.querySelector("#exp").classList.add("invisible");
-         document.querySelector("#ski").classList.add("invisible");
-         document.querySelector("#pro").classList.add("invisible");
-         document.querySelector("#con").classList.add("invisible");
+    
+         $("#top").addClass("invisible");
+         $("#abo").addClass("invisible");
+         $("#exp").addClass("invisible");
+         $("#ski").addClass("invisible");
+         $("#pro").addClass("invisible");
+         $("#con").addClass("invisible");
 
         switch(screen)
         {
-           case 1: document.querySelector("#top").classList.remove("invisible"); break;
+           case 1: $("#top").removeClass("invisible"); break;
 
-           case 2: document.querySelector("#abo").classList.remove("invisible"); break;
+           case 2: $("#abo").removeClass("invisible"); break;
             
-           case 3: document.querySelector("#exp").classList.remove("invisible"); break;
+           case 3: $("#exp").removeClass("invisible"); break;
 
-           case 4: document.querySelector("#ski").classList.remove("invisible"); break;
+           case 4: $("#ski").removeClass("invisible"); break;
 
-           case 5: document.querySelector("#pro").classList.remove("invisible"); break;
+           case 5: $("#pro").removeClass("invisible"); break;
 
-           case 6: document.querySelector("#con").classList.remove("invisible"); break;
+           case 6: $("#con").removeClass("invisible"); break;
         }
         
 }
 
 
-document.querySelector(".btnHome").onclick = function() {switchScreen(1)};
-document.querySelector(".btnAbo").onclick = function() {switchScreen(2)};
-document.querySelector(".btnExp").onclick = function() {switchScreen(3)};
-document.querySelector(".btnSki").onclick = function() {switchScreen(4)};
-document.querySelector(".btnPro").onclick = function() {switchScreen(5)};
-document.querySelector(".btnCon").onclick = function() {switchScreen(6)};
-document.querySelector(".title-btn").onclick = function() {switchScreen(6)};
+$(".btnHome").click(function() {switchScreen(1)});
+$(".btnAbo").click(function() {switchScreen(2)});
+$(".btnExp").click(function() {switchScreen(3)});
+$(".btnSki").click(function() {switchScreen(4)});
+$(".btnPro").click(function() {switchScreen(5)});
+$(".btnCon").click(function() {switchScreen(6)});
+$(".title-btn").click(function() {switchScreen(6)});
