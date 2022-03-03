0914af6482e8b77ce6f6f60619139349bfb4d877
Michael Gately <michaedavidgately@gmail.com>
2022-03-03 14:20:11 +0100

    more content, almost there to first draft

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
 function switchScreen(screen) {
-    
-         $("#top").addClass("invisible");
-         $("#abo").addClass("invisible");
-         $("#exp").addClass("invisible");
-         $("#ski").addClass("invisible");
-         $("#pro").addClass("invisible");
-         $("#con").addClass("invisible");
 
-        switch(screen)
-        {
-           case 1: $("#top").removeClass("invisible"); break;
+   $("#top").addClass("invisible");
+   $("#abo").addClass("invisible");
+   $("#exp").addClass("invisible");
+   $("#ski").addClass("invisible");
+   $("#pro").addClass("invisible");
+   $("#con").addClass("invisible");
 
-           case 2: $("#abo").removeClass("invisible"); break;
-            
-           case 3: $("#exp").removeClass("invisible"); break;
+   switch (screen) {
+      case 1:
+         $("#top").removeClass("invisible");
+         break;
 
-           case 4: $("#ski").removeClass("invisible"); break;
+      case 2:
+         $("#abo").removeClass("invisible");
+         break;
 
-           case 5: $("#pro").removeClass("invisible"); break;
+      case 3:
+         $("#exp").removeClass("invisible");
+         break;
 
-           case 6: $("#con").removeClass("invisible"); break;
-        }
-        
+      case 4:
+         $("#ski").removeClass("invisible");
+         break;
+
+      case 5:
+         $("#pro").removeClass("invisible");
+         break;
+
+      case 6:
+         $("#con").removeClass("invisible");
+         break;
+   }
+
 }
 
 
-$(".btnHome").click(function() {switchScreen(1)});
-$(".btnAbo").click(function() {switchScreen(2)});
-$(".btnExp").click(function() {switchScreen(3)});
-$(".btnSki").click(function() {switchScreen(4)});
-$(".btnPro").click(function() {switchScreen(5)});
-$(".btnCon").click(function() {switchScreen(6)});
-$(".title-btn").click(function() {switchScreen(6)});
+$(".btnHome").click(function () {
+   switchScreen(1)
+});
+$(".btnAbo").click(function () {
+   switchScreen(2)
+});
+$(".btnExp").click(function () {
+   switchScreen(3)
+});
+$(".btnSki").click(function () {
+   switchScreen(4)
+});
+$(".btnPro").click(function () {
+   switchScreen(5)
+});
+$(".btnCon").click(function () {
+   switchScreen(6)
+});
+$(".title-btn").click(function () {
+   switchScreen(6)
+});
